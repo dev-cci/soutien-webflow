fc41b954b02e90703338e66b23798aa68e0a01dd
svadhis <imeian@gmail.com>
2022-02-09 10:37:48 +0100

    step 2 ok

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+
+include('./database.php');
+$req = $bdd->prepare("SELECT * FROM users WHERE username = 'BatMan'");
+$req->execute();
+
+$user = $req->fetch();
+$userId = $user['id'];
+
+$req = $bdd->prepare("SELECT * FROM comments WHERE user_id = '$userId'");
+$req->execute();
+
+$comments = $req->fetchAll();
+
+$user['comments'] = $comments;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,13 +30,17 @@
     </div>
     <div class="user-profile">
         <div class="user-info">
-            <h3>BatMan</h3>
+            <h3><?= $user['username'] ?></h3>
             <img src="https://www.lego.com/cdn/cs/set/assets/blt167d8e20620e4817/DC_-_Character_-_Details_-_Sidekick-Standard_-_Batman.jpg?fit=crop&format=jpg&quality=80&width=800&height=426&dpr=1" alt="">
         </div>
         <div class="user-comments">
             <h4>Commentaires</h4>
-            <p>Ah ah ah :D</p>
-            <p>I'm THE BatMan !</p>
+
+            <?php foreach ($user['comments'] as $comment) : ?>
+
+                <p><?= $comment['message'] ?></p>
+
+            <?php endforeach; ?>
         </div>
     </div>
 
