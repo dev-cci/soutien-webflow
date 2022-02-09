58067143714acfd06b41e22469f0edd83524c02b
svadhis <imeian@gmail.com>
2022-02-09 10:59:06 +0100

    step 4 ok

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
-<?php
-
-include('./database.php');
-$req = $bdd->prepare("SELECT * FROM users WHERE username = 'BatMan'");
-$req->execute();
-
-$user = $req->fetch();
-$userId = $user['id'];
-
-$req = $bdd->prepare("SELECT * FROM comments WHERE user_id = '$userId'");
-$req->execute();
-
-$comments = $req->fetchAll();
-
-$user['comments'] = $comments;
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -30,19 +12,6 @@ $user['comments'] = $comments;
         <h2>Mon profil</h2>
     </div>
     <div class="user-profile">
-        <div class="user-info">
-            <h3><?= $user['username'] ?></h3>
-            <img src="https://www.lego.com/cdn/cs/set/assets/blt167d8e20620e4817/DC_-_Character_-_Details_-_Sidekick-Standard_-_Batman.jpg?fit=crop&format=jpg&quality=80&width=800&height=426&dpr=1" alt="">
-        </div>
-        <div class="user-comments">
-            <h4>Commentaires</h4>
-
-            <?php foreach ($user['comments'] as $comment) : ?>
-
-                <p><?= $comment['message'] ?></p>
-
-            <?php endforeach; ?>
-        </div>
     </div>
 
     <footer>
--- script.js
+++ script.js
@@ -1,26 +1,21 @@
 fetch('./get_user.php')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(user => {
+        let html = `
+            <div class="user-info">
+                <h3>${user.username}</h3>
+                <img src="https://www.lego.com/cdn/cs/set/assets/blt167d8e20620e4817/DC_-_Character_-_Details_-_Sidekick-Standard_-_Batman.jpg?fit=crop&format=jpg&quality=80&width=800&height=426&dpr=1" alt="">
+            </div>
+            <div class="user-comments">
+                <h4>Commentaires</h4>
+        `
 
+        user.comments.forEach(comment => {
+            html += `<p>${comment.message}</p>`
+        });
 
+        html += `</div></div>`
 
-// EQUIVALENT TO
-fetch('./get_user.php')
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(data) {
-        console.log(data)
-    })
-
-
-
-// EQUIVALENT TO
-fetch('./get_user.php')
-    .then(function(response) {
-        return response.text()
-    })
-    .then(function(data) {
-        jsonData = JSON.parse(data)
-        console.log(jsonData)
+        const userProfile = document.querySelector('.user-profile')
+        userProfile.innerHTML = html;
     })
