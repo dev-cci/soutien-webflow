550c32eb0e2f3884e109b5a9a5c26be921d79fb9
svadhis <imeian@gmail.com>
2022-02-09 10:26:15 +0100

    step 1 ok

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Webflow</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="nav">
+        <h2>Mon profil</h2>
+    </div>
+    <div class="user-profile">
+        <div class="user-info">
+            <h3>BatMan</h3>
+            <img src="https://www.lego.com/cdn/cs/set/assets/blt167d8e20620e4817/DC_-_Character_-_Details_-_Sidekick-Standard_-_Batman.jpg?fit=crop&format=jpg&quality=80&width=800&height=426&dpr=1" alt="">
+        </div>
+        <div class="user-comments">
+            <h4>Commentaires</h4>
+            <p>Ah ah ah :D</p>
+            <p>I'm THE BatMan !</p>
+        </div>
+    </div>
+
+    <footer>
+        Copyright
+    </footer>
+
+</body>
+</html>
